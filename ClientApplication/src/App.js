ab0dfbf2bf604d27717712d3f662a5ca5627538e
Nikhil Aggarwal <nikhil0929@gmail.com>
2021-12-20 22:15:54 -0800

    Implemented list view of team members in database
    Uses the list_members() API endpoint specified on the Django backend

---
+++ ClientApplication/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ListMembers from "./components/ListMembers";
+
+function App() {
+  return (
+    <div className="App">
+      <ListMembers />
+    </div>
+  );
+}
+
+export default App;
